d505a5c026d51542c5ddf76cb73510499fa50e7d
sheppardrs <sheppard.r.somers.19@dartmouth.edu>
2018-08-07 16:43:06 -0400

    added rough chat with conversations model, can start a convo or select an old on

--- src/components/chat.js
+++ src/components/chat.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import socketIOc from 'socket.io-client';
 
+// this should probably be decomposed into a few different components
+
+
 // this component is based on the hackermoon article "A simple messaging app with react native and socket.io"
 // https://hackernoon.com/a-simple-messaging-app-with-react-native-and-socket-io-e1cae3df7bda
 const chatserverURL = 'http://localhost:3000';
+
 
 class Chat extends React.Component {
   constructor(props) {
@@ -11,8 +15,10 @@ class Chat extends React.Component {
 
     this.state = {
       messages: [],
+      conversations: [],
       message: '',
-      to: '',
+      new: '', // username for new conversation
+      to: '', // should be conversation
     };
 
     this.username = localStorage.getItem('username');
@@ -27,23 +33,49 @@ class Chat extends React.Component {
     this.handleSend = this.handleSend.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleLoad = this.handleLoad.bind(this);
+    this.handleConvos = this.handleConvos.bind(this);
+    this.selectConvo = this.selectConvo.bind(this);
+    this.startConvo = this.startConvo.bind(this);
 
     this.socket.on('message', this.handleRec);
     this.socket.on('messages', this.handleLoad);
+    this.socket.on('convos', this.handleConvos);
+  }
+
+  // handle receiving the list of conversations that this user is in
+  handleConvos(convos) {
+    console.log('received convos', convos);
+    this.setState({
+      conversations: convos,
+    });
+  }
+
+  // select a conversation
+  // send a request for the messages of that conversation
+  selectConvo(conversation, e) {
+    console.log('selecting convo: ', conversation);
+    this.setState({ to: conversation });
+    this.socket.emit('convo', conversation);
+    e.preventDefault();
   }
 
   // handle receiving the past messages
   handleLoad(history) {
+    console.log('received messages', history);
     this.setState({
       messages: history,
     });
   }
 
   // handle receiving a message
+  // check that it is for the current conversation
+  // TODO: deal with receiving other messages from other conversations
   handleRec(message) {
-    this.setState(prevState => ({
-      messages: [...prevState.messages, message],
-    }));
+    if (message.to === this.state.to._id) {
+      this.setState(prevState => ({
+        messages: [...prevState.messages, message],
+      }));
+    }
   }
 
   handleSend(e) {
@@ -59,6 +91,10 @@ class Chat extends React.Component {
     e.preventDefault();
   }
 
+  startConvo(e) {
+    this.socket.emit('startconvo', this.state.new);
+  }
+
   handleChange(e) {
     const field = e.target.name;
     this.setState({ [field]: e.target.value });
@@ -68,16 +104,54 @@ class Chat extends React.Component {
   render() {
     return (
       <div className="chat">
-        To:
-        <input
-          type="text"
-          name="to"
-          placeholder="Username"
-          value={this.state.to}
-          onChange={this.handleChange}
-          className="message-enter"
-        />
-        {this.state.messages.map((message) => {
+        <div className="chat-controls">
+          <div className="chat-conversations">
+          Your Recent Conversations:
+            {this.state.conversations.map((conversation) => {
+            // console.log(this.state.request, post.request);
+              return (
+                <button
+                  className="chat-conversation"
+                  key={conversation._id}
+                //  value={conversation}
+                  onClick={e => this.selectConvo(conversation, e)}
+                >
+                  {conversation.participants.map((participant) => {
+                    if (participant === this.username) {
+                      return (<div key={participant} />);
+                    } else {
+                      return (<div key={participant} className="chat-conversation-part">{participant}</div>);
+                    }
+                  })}
+                </button>
+              );
+            })}
+          </div>
+          <div className="chat-startconvo">
+            <form
+              onSubmit={this.startConvo}
+              className="startconvo-form"
+            >
+          New Conversation:
+              <input
+                type="text"
+                name="new"
+                placeholder="Username"
+                value={this.state.new}
+                onChange={this.handleChange}
+                className="message-enter"
+              />
+              <button
+                type="submit"
+                className="save-button"
+              >
+              Start
+              </button>
+            </form>
+          </div>
+        </div>
+        <div className="chat-messages">
+          {this.state.messages.map((message) => {
           // console.log(this.state.request, post.request);
             return (
               <div
@@ -97,26 +171,27 @@ class Chat extends React.Component {
               </div>
             );
           })}
-        <form
-          onSubmit={this.handleSend}
-          className="message-form"
-        >
-          Message:
-          <input
-            type="text"
-            name="message"
-            placeholder="Say something?"
-            value={this.state.message}
-            onChange={this.handleChange}
-            className="message-enter"
-          />
-          <button
-            type="submit"
-            className="save-button"
+          <form
+            onSubmit={this.handleSend}
+            className="message-form"
           >
+          Message:
+            <input
+              type="text"
+              name="message"
+              placeholder="Say something?"
+              value={this.state.message}
+              onChange={this.handleChange}
+              className="message-enter"
+            />
+            <button
+              type="submit"
+              className="save-button"
+            >
               Send
-          </button>
-        </form>
+            </button>
+          </form>
+        </div>
       </div>
     );
   }
