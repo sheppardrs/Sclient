a23611158e4aad5197508ab395ac9fc877fa0493
sheppardrs <sheppard.r.somers.19@dartmouth.edu>
2018-08-14 01:08:24 -0400

    only shows new message number when > 0

--- src/components/nav.js
+++ src/components/nav.js
@@ -25,7 +25,7 @@ const Nav = (props) => {
           <li><NavLink className="navlink" to="/" exact>Home</NavLink></li>
           <li><NavLink className="navlink" to="/posts/new">Add Post</NavLink></li>
           <li><NavLink className="navlink" to="/about">About</NavLink></li>
-          <li><NavLink className="navlink" to="/chat">Chat<div id="new-mess-notification">{props.notifications}</div></NavLink></li>
+          <li><NavLink className="navlink" to="/chat">Chat{props.notifications > 0 ? <div id="new-mess-notification">{props.notifications}</div> : <div />}</NavLink></li>
           <li><NavLink id="signout-link" className="navlink" to="/" onClick={(e) => { props.signoutUser(props.history); }}>Sign Out</NavLink></li>
         </ul>
       </nav>
